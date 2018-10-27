35327a0215d72e376d0703b614d04f851651ac8d
Einherjarn <31859362+Zardian@users.noreply.github.com>
2018-10-27 20:34:19 +0300

    files

---
+++ Bossnames/bossnames.en.lua
@@ -0,0 +1,172 @@
+AtlasLootBossNames = {
+    
+	BlackrockDepths = {
+        "Lord Roccor (BRD)";
+		"Kharan Mighthammer (BRD)";
+		"Commander Gor'shak (BRD)";
+		"Marshal Windsor (BRD)";
+		"High Interrogator Gerstahn (BRD)";
+		"Arena, Houndmaster & Theldren (BRD)";
+		"Mon. of Franclorn Forgewright (BRD)";
+		"Pyromancer Loregrain (BRD)";
+		"The Vault (BRD)";
+		"Fineous Darkvire (BRD)";
+		"The Black anvil (BRD)";
+		"Lord Incendius (BRD)";
+		"Bael'Gar (BRD)";
+		"Shadowforge Lock (BRD)";
+		"General Angerforge (BRD)";
+		"Golem Lord Argelmach (BRD)";
+		"The Grim Guzzler (BRD)";
+		"Ambassador Flamelash (BRD)";
+		"Panzor the Invincible (Rare) (BRD)";
+		"Summoner's Tomb (BRD)";
+		"The Lyceum (BRD)";
+		"Magmus (BRD)";
+		"Emperor Dragan Thaurissan (BRD)";
+		"Princess Moira Bronzebeard (BRD)";
+		"The Black Forge (BRD)";
+		"Molten Core (BRD)";
+        };
+    BlackrockSpireLower = {
+        "Highlord Omokk (LBRS)";
+        "Spirestone Battle Lord (LBRS)";
+        "Vosh'gajin (LBRS)";
+        "War Master Voone (LBRS)";
+        "Mor Grayhoof (LBRS)";
+        "Mother Smolderweb (LBRS)";
+        "Crystal Fang (LBRS)";
+        "Doomhowl (LBRS)";
+        "Zigris (LBRS)";
+        "Gizrul the Slavener (LBRS)";
+        "Halycon (LBRS)";
+        "Overlord Wyrmthalak (LBRS)";
+        "Grimaxe (LBRS)";
+        "Spirestone Butcher (LBRS)";
+        };
+    
+    BlackrockSpireUpper = {
+        "Pyroguard Emberseer (UBRS)";
+        "Solakar Flamewreath (UBRS)";
+        "Father Flame (UBRS)";
+        "Jed Runewatcher (UBRS)";
+        "Goraluk Anvilcrack (UBRS)";
+        "Rend Blackhand (UBRS)";
+        "Gyth (UBRS)";
+        "The Beast (UBRS)";
+        "Valthalak (UBRS)";
+        "Drakkisath (UBRS)";
+        };
+    
+    BlackwingLair = {
+	    "Razorgore";
+	    "Vaelastrasz";
+	    "Broodlord Lashlayer";
+	    "Firemaw";
+	    "Ebonroc";
+	    "Flamegor";
+	    "Chromaggus";
+	    "Nefarian";
+        };
+        
+    Common = {
+        "Trash Mobs";
+        };
+    
+    MoltenCore = {
+	    "Lucifron";
+	    "Magmadar";
+	    "Gehennas";
+	    "Garr";
+	    "Shazzrah";
+	    "Geddon";
+	    "Golemagg";
+	    "Sulfuron";
+	    "Majordomo";
+	    "Ragnaros";
+        };
+        
+    Naxxramas = {
+        "Patchwerk";
+        "Grobbulus";
+        "Gluth";
+        "Thaddius";
+        "Anub'Rekhan";
+        "Faerlina";
+        "Maexxna";
+        "Instructor Razuvious";
+        "Gothik the Harvester";
+        "The Four Horsemen";
+        "Noth the Plaguebringer";
+        "Heigan the Unclean";
+        "Loatheb";
+        "Sapphiron";
+        "Kel'Thuzard";
+        };
+        
+    OnyxiasLair = {
+        "Onyxia";
+        };
+    
+    Scholomance = {
+        "Blood Steward of Kirtonos (SCHOLO)";
+        "Kirtonos (SCHOLO)";
+        "Jandice Barov (SCHOLO)";
+        "Rattlegore (SCHOLO)";
+        "Death Knight Darkreaver (SCHOLO)";
+        "Marduk Blackpool (SCHOLO)";
+        "Vectus (SCHOLO)";
+        "Ras Frostwhisper (SCHOLO)";
+        "Kormok (SCHOLO)";
+        "Instructor Malicia (SCHOLO)";
+        "Doctor Theolen Krastinov (SCHOLO)";
+        "Lorekeeper Polkelt (SCHOLO)";
+        "The Ravenian (SCHOLO)";
+        "Lord Alexei Barov (SCHOLO)";
+        "Lady Illucia Barov (SCHOLO)";
+        "Darkmaster Gandling (SCHOLO)";
+       };
+    
+    Stratholme = {
+       "Skul (STRAT)";
+       "Stratholme Courier (STRAT)";
+       "Fras Siabi (STRAT)";
+       "Hearthsinger Forresten (STRAT)";
+       "The Unforgiven (STRAT)";
+       "Timmy the Cruel (STRAT)";
+       "Cannon Master Willey (STRAT)";
+       "Archivist Galford (STRAT)";
+       "Balnazzar (STRAT)";
+       "Sothos & Jarien (STRAT)";
+       "Aurius (STRAT)";
+       "Stonespine (STRAT)";
+       "Baroness Anastari (STRAT)";
+       "Nerub'enkan (STRAT)";
+       "Maleki the Pallid (STRAT)";
+       "Magistrate Barthilas (STRAT)";
+       "Ramstein the Gorger (STRAT)";
+       "Baron Rivendare (STRAT)";
+       };
+        
+    TheRuinsofAhnQiraj = {
+	    "Kurinnaxx";
+	    "Rajaxx";
+	    "Moam";
+	    "Buru";
+	    "Ayamiss";
+	    "Ossirian";
+        };
+        
+    TheTempleofAhnQiraj = {
+	    "Skeram";
+	    "Bug Family";
+	    "Sartura";
+	    "Fankriss";
+	    "Viscidus";
+	    "Huhuran";
+	    "Emperors";
+	    "Ouro";
+	    "CThun";
+        };
+    };
+        
